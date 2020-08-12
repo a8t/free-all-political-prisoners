7d261422d0feb3343500775393db22c0c52efa9f
Andy <andy@gettimesaved.com>
2020-08-12 17:40:13 -0400

    Format files

--- studio/resolvePreviewUrl.js
+++ studio/resolvePreviewUrl.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 
-export default function resolvePreviewUrl(document) {
+export default function resolvePreviewUrl (document) {
   const baseUrl = env === 'development' ? 'http://localhost:8000' : ''
   switch (document._type) {
     case 'route':
